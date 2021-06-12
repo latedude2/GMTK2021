fee2c6dd1bbcfdf6f5fe5b5630ccbce41e9548e3
latedude2 <simasc10@gmail.com>
2021-06-12 15:02:22 +0200

    Make stragglers look towards circle

--- Assets/Scripts/Straggler.cs
+++ Assets/Scripts/Straggler.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class Straggler : MonoBehaviour
 {
-    
     public float speed = 1;
     public Transform circle;
     public AudioSource audioSource;
@@ -28,7 +27,10 @@ public class Straggler : MonoBehaviour
 
         // move sprite towards the target location
         transform.position = Vector3.MoveTowards(transform.position, circle.position, step);
+        Quaternion rotation = Quaternion.LookRotation(circle.transform.position - transform.position, transform.TransformDirection(Vector3.up));
+        transform.rotation = new Quaternion(0, 0, rotation.z, rotation.w);
     }
+
 
     // called when the cube hits the floor
     void OnTriggerEnter2D(Collider2D col)
