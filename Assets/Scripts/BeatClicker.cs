16ceb0b7cf69a672a38a65dbab4df9d05db3a08c
Jokubas <jokubas@shape.dk>
2021-06-12 10:14:16 +0200

    added timer, score and beat clicker initial scripts

---
+++ Assets/Scripts/BeatClicker.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeatClicker : MonoBehaviour
+{
+
+
+
+    public void OnBeatButtonClicked()
+    {
+        ScoreManager.ProcessClick(Timer.timeLeftToBeat);
+    }
+}
